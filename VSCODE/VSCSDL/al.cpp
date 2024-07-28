49a9b9238707cf7ee4e024949acd23d7f527f05e
dulred <dulred@qq.com>
2024-07-29 01:14:42 +0800

    1

--- VSCODE/VSCSDL/al.cpp
+++ VSCODE/VSCSDL/al.cpp
@@ -1,59 +1,46 @@
-/*
- * @Author: dulred dulred@qq.com
- * @Date: 2024-07-20 18:52:43
- * @LastEditors: dulred dulred@qq.com
- * @LastEditTime: 2024-07-25 00:11:29
- * @FilePath: \VSCSDL\my_test.cpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 #include <vector>
 #include <iostream>
+#include <map>
 
 using namespace std;
 
-class Solution {
-public:
-    int candy(vector<int>& ratings) {
-        int len = ratings.size();
-        int res = len;
-        int i = 1;
-        while (i < len)
-        {
-            if (ratings[i-1] > ratings [i])
-            {
-                res+=1;
-                if (i+1 != len)
-                {
-                    if (ratings[i]>ratings[i+1])
-                    {
-                        res+=1;
-                    }
-                    
-                }
-                return res+=1;
-            }
-            
-            // int count = 0;
-            // int j = i;
-            // while (j < len)
-            // {
-            //     cout << "sss";
-                
-            //     j++;
-            // }
-            ++i;
-        }
-        return res;
-    }
-};
+std::map RE_JsonExtract (const string& json) {
+
+    std::map<string, string> myMap;
+    myMap["code"] = "201";
+    myMap["id"] = "VZQslvjoM1";
+
+    return myMap;
+}
 
 int main (int, char*[])
-{
-    vector<int> cc = {1,0,2};
-    Solution* solution = new Solution();
-    int len = solution->candy(cc);
-    cout << len <<endl;
-    delete solution;
+{ 
+
+    std::string ggb = "{\n"
+                      "    \"code\":201,\n"
+                      "    \"desc\":\"SUCCESS\",\n"
+                      "    \"data\":{\n"
+                      "        \"id\":\"VZQslvjoM1\",\n"
+                      "        \"tsp\": 1616496562\n"
+                      "    } \n"
+                      "}";
+
+                 
+    std::map<string, string> myMap;
+    myMap["code"] = "201";
+    myMap["id"] = "VZQslvjoM1";
+
+    std::cout << "Code: " << myMap["code"] << std::endl;
+    std::cout << "ID: " << myMap["id"] << std::endl;
+
+    // 解析json字符串数据
+     RE_JsonExtract(ggb);
+
+    // {} 用map集合 []用vector 获取格式 传入解析的  jsonText[code][id] 这样获取 ,多重map
+    cout <<jsonText<<endl;
+
+    
+
     return 0;
 
 }
